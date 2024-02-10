3e7ed09e13c7d6847ea293792986992f755ba01c
Leina Gray <abad.alleina@gmail.com>
2024-02-10 22:59:30 +0800

    ADD DATABASE & FAMILY MEMBER LIST PAGE: add DatabaseHelper for accessing SQLite. The DatabaseHelper creates a new table called personal info. It consists of columns First Name, Middle Name and Last Name. After clicking sign up, the app goes to family member list view. The family member list view consists of an add button and an empty recyclerview of the family member list.

---
+++ app/src/main/java/DatabaseHelper.java
@@ -0,0 +1,38 @@
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private Context context;
+    private static final String DATABASE_NAME = "FamilyVault.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_NAME = "personal_info";
+    private static final String ID = "ID";
+    private static final String FIRST_NAME = "first_name";
+    private static final String MIDDLE_NAME = "middle_name";
+    private static final String LAST_NAME = "last_name";
+    public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query =
+                "CREATE TABLE " + TABLE_NAME +
+                        "(" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                            + FIRST_NAME + " TEXT,"
+                            + MIDDLE_NAME + " TEXT,"
+                            + LAST_NAME + " TEXT"
+                        + ");";
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}
