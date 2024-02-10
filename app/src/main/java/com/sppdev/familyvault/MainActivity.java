1ff1397050263ff30cc1e7a22caef761a1122d6d
Leina Gray <abad.alleina@gmail.com>
2024-02-10 21:49:19 +0800

    Get First Name: get first name from edit text field and then display the name on screen using text view. Use toast and log or print to log to check if function is called.

--- app/src/main/java/com/sppdev/familyvault/MainActivity.java
+++ app/src/main/java/com/sppdev/familyvault/MainActivity.java
@@ -2,6 +2,11 @@ package com.sppdev.familyvault;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void getFirstName(View v){
+        EditText t = findViewById(R.id.first_name);
+        String input = t.getText().toString();
+        ((TextView)findViewById(R.id.display)).setText(input);
+
+        Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show();
+        Log.d(null, "onCreate called.");
+
     }
 }
