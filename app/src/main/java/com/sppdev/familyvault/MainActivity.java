bb3ca1494b420fe2803d5b6b17d1ef32140b3dd5
Leina Gray <abad.alleina@gmail.com>
2024-02-10 22:59:03 +0800

    ADD DATABASE & FAMILY MEMBER LIST PAGE: add DatabaseHelper for accessing SQLite. The DatabaseHelper creates a new table called personal info. It consists of columns First Name, Middle Name and Last Name. After clicking sign up, the app goes to family member list view. The family member list view consists of an add button and an empty recyclerview of the family member list.

--- app/src/main/java/com/sppdev/familyvault/MainActivity.java
+++ app/src/main/java/com/sppdev/familyvault/MainActivity.java
@@ -1,6 +1,8 @@
 package com.sppdev.familyvault;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -8,7 +10,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+    RecyclerView family_member_list;
+    FloatingActionButton add_family_member;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +28,21 @@ public class MainActivity extends AppCompatActivity {
         String input = t.getText().toString();
         ((TextView)findViewById(R.id.display)).setText(input);
 
+        showFamilyList(v);
+
+
         Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show();
         Log.d(null, "onCreate called.");
+    }
+
+    public void showFamilyList(View v) {
+        setContentView(R.layout.family_members);
+
+    }
+
+    public void addFamilyMember(View v) {
+        family_member_list = findViewById(R.id.family_member_list);
+        add_family_member = findViewById(R.id.add_family_member);
 
     }
 }
